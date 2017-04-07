a05c77e4eb8ef8f4a87f528b2a5dc931afb91e0d
Daniel Hartl <daniel_hartl@gmx.at>
2017-04-07 23:39:37 +0200

    fix test

--- app/src/androidTest/java/at/c02/tempus/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/at/c02/tempus/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.nitinkg.nearbyrestaurant", appContext.getPackageName());
+        assertEquals("at.c02.tempus", appContext.getPackageName());
     }
 }
