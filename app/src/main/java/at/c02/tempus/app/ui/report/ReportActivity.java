6be59cc63d267d4e1523446c984fd4c0a760b137
MichaelGmoser <michael.gmoser@edu.campus02.at>
2017-05-12 20:18:43 +0200

    ReportActivity: onBtnDateStartClick(),showItems ReportPresenter: startDate, List<BookingsEntity> report_activity.xml --> Button, CalenderView

--- app/src/main/java/at/c02/tempus/app/ui/report/ReportActivity.java
+++ app/src/main/java/at/c02/tempus/app/ui/report/ReportActivity.java
@@ -1,12 +1,25 @@
 package at.c02.tempus.app.ui.report;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.widget.Button;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
 
 import at.c02.tempus.R;
 import at.c02.tempus.app.ui.bookinglist.BookingActivityPresenter;
+import at.c02.tempus.app.ui.bookinglist.BookingListItem;
+import at.c02.tempus.db.entity.BookingEntity;
+import at.c02.tempus.utils.DateUtils;
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import nucleus.factory.RequiresPresenter;
 import nucleus.view.NucleusAppCompatActivity;
 
@@ -19,6 +32,10 @@ public class ReportActivity extends NucleusAppCompatActivity<ReportPresenter> {
     @BindView(R.id.toolbar)
     protected Toolbar toolbar;
 
+    @BindView(R.id.btnDateStart)
+    protected Button btnDateStart;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,5 +44,36 @@ public class ReportActivity extends NucleusAppCompatActivity<ReportPresenter> {
         ButterKnife.bind(this);
 
         setSupportActionBar(toolbar);
+    }
+
+    @OnClick(R.id.btnDateStart)
+    protected void onBtnDateStartClick() {
+        Date date = getPresenter().getModel().getBeginDate();
+        if (date == null) {
+            date = new Date();
+        }
+        Calendar calendar = DateUtils.dateToCalendar(date);
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this, 0,
+                (view, year, month, dayOfMonth) ->
+                        getPresenter().setStartDate(year, month, dayOfMonth),
+                calendar.get(Calendar.YEAR),
+                calendar.get(Calendar.MONTH),
+                calendar.get(Calendar.DAY_OF_MONTH));
+        datePickerDialog.getDatePicker().setMaxDate(new Date().getTime());
+        datePickerDialog.show();
+    }
+
+    public void showItems(List<BookingEntity> bookings) {
+        List<BookingListItem> listItems = new ArrayList<>();
+        for (BookingEntity booking : bookings) {
+            String projectName = booking.getProject() != null ? booking.getProject().getName() : "";
+            listItems.add(new BookingListItem(booking.getId(),
+                    projectName,
+                    booking.getBeginDate(),
+                    booking.getEndDate()));
+        }
+        Log.d("CREATION",listItems.toString());
+
+
     }
 }
--- app/src/main/java/at/c02/tempus/app/ui/report/ReportPresenter.java
+++ app/src/main/java/at/c02/tempus/app/ui/report/ReportPresenter.java
@@ -1,9 +1,15 @@
 package at.c02.tempus.app.ui.report;
 
+import java.util.Date;
+import java.util.List;
+
 import javax.inject.Inject;
 
 import at.c02.tempus.app.TempusApplication;
+import at.c02.tempus.app.ui.bookinglist.FragmentBookingList;
+import at.c02.tempus.db.entity.BookingEntity;
 import at.c02.tempus.service.ReportService;
+import at.c02.tempus.utils.DateUtils;
 import nucleus.presenter.Presenter;
 
 /**
@@ -15,6 +21,10 @@ public class ReportPresenter extends Presenter<ReportActivity> {
     @Inject
     protected ReportService reportService;
 
+    private BookingEntity model = new BookingEntity();
+
+    //private List<BookingEntity> bookings;
+
     public ReportPresenter() {
         TempusApplication.getApp().getApplicationComponents().inject(this);
     }
@@ -23,4 +33,26 @@ public class ReportPresenter extends Presenter<ReportActivity> {
     public void createReport() {
       //  reportService.findBookings();
     }
+
+
+    public BookingEntity getModel() {
+        return model;
+    }
+
+    public void setModel(BookingEntity model) {
+        this.model = model;
+    }
+
+    public void setStartDate(int year, int month, int day) {
+        Date newBeginDate = DateUtils.setDate(model.getBeginDate(), year, month, day);
+        model.setBeginDate(newBeginDate);
+    }
+/*
+    @Override
+    protected void onTakeView(ReportActivity reportActivity) {
+        super.onTakeView(reportActivity);
+        if (bookings != null) {
+            getView().showItems(bookings);
+        }
+    }*/
 }
