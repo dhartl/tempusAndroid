6d8efec5f8b106dd81ba2ab1fa035aced7800ec9
Philipp <philippg@outlook.at>
2017-05-12 18:33:21 +0200

    initial commit to recordButtonfragment

--- app/src/main/java/at/c02/tempus/app/ui/booking/FragmentBooking.java
+++ app/src/main/java/at/c02/tempus/app/ui/booking/FragmentBooking.java
@@ -1,15 +1,30 @@
 package at.c02.tempus.app.ui.booking;
 
 
+import at.c02.tempus.app.ui.bookinglist.BookingListItem;
+import eu.davidea.flexibleadapter.FlexibleAdapter;
+import nucleus.factory.RequiresPresenter;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
+import java.util.List;
 import at.c02.tempus.R;
-import nucleus.factory.RequiresPresenter;
+import at.c02.tempus.db.entity.ProjectEntity;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import nucleus.view.NucleusSupportFragment;
+
 
 /**
  * Created by Daniel on 09.04.2017.
@@ -17,9 +32,37 @@ import nucleus.view.NucleusSupportFragment;
 @RequiresPresenter(FragmentBookingPresenter.class)
 public class FragmentBooking extends NucleusSupportFragment<FragmentBookingPresenter> {
 
+    @BindView(R.id.cbProject)
+    protected Spinner cbProject;
+
+    @BindView(R.id.btnRecordButton)
+    protected Button btnRecordButton;
+
+    private ArrayAdapter<ProjectEntity> adapter;
+
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_booking, container, false);
+        View root = inflater.inflate(R.layout.fragment_booking, container, false);
+        ButterKnife.bind(this,root);
+        return root;
     }
+
+    @OnClick(R.id.btnRecordButton)
+    protected  void onRecordClick()
+    {
+        //do something...
+    }
+
+
+    public void updateProjects(List<ProjectEntity> projects) {
+        adapter.clear();
+        adapter.addAll(projects);
+    }
+    public void onError(Throwable error) {
+        Toast.makeText(this.getContext(), error.getMessage(), Toast.LENGTH_LONG).show();
+    }
+
+
 }
--- app/src/main/java/at/c02/tempus/app/ui/booking/FragmentBookingPresenter.java
+++ app/src/main/java/at/c02/tempus/app/ui/booking/FragmentBookingPresenter.java
@@ -1,10 +1,97 @@
 package at.c02.tempus.app.ui.booking;
 
-import nucleus.presenter.Presenter;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 
+import org.greenrobot.eventbus.EventBus;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import at.c02.tempus.app.TempusApplication;
+import at.c02.tempus.db.entity.BookingEntity;
+import at.c02.tempus.db.entity.ProjectEntity;
+import at.c02.tempus.service.BookingService;
+import at.c02.tempus.service.ProjectService;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import nucleus.presenter.Presenter;
 /**
  * Created by Daniel Hartl on 13.04.2017.
+ * Modified by Philipp^2
  */
 
-public class FragmentBookingPresenter extends Presenter {
+public class FragmentBookingPresenter extends Presenter<FragmentBooking> {
+
+    private BookingEntity model = new BookingEntity();
+
+    @Inject
+    protected BookingService bookingService;
+
+    @Inject
+    protected ProjectService projectService;
+
+    @Inject
+    protected EventBus eventBus;
+
+    private List<ProjectEntity> projects;
+
+    //necessary?
+    private Throwable error;
+
+    public FragmentBookingPresenter() {
+        TempusApplication.getApp().getApplicationComponents().inject(this);
+    }
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedState) {
+        super.onCreate(savedState);
+        eventBus.register(this);
+
+        projectService.getProjects()
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(result -> {
+                            this.projects = result;
+                            publishProjects();
+                        }, error -> {
+                            this.error = error;
+                            publishProjects();
+                        }
+                );
+    }
+
+    private void publishProjects() {
+        if (getView() != null) {
+            if (projects != null) {
+                getView().updateProjects(projects);
+            } else {
+                getView().onError(error);
+            }
+        }
+    }
+
+
+
+    public BookingEntity getModel() {
+        return model;
+    }
+
+    public void setModel(BookingEntity model) {
+        this.model = model;
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        eventBus.unregister(this);
+        super.onDestroy();
+    }
+
+    private void onError(Throwable error) {
+        this.error = error;
+        if (getView() != null) {
+            getView().onError(error);
+        }
+    }
+
 }
