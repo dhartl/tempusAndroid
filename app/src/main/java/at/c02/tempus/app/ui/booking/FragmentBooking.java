bf56056d13a55e46f3da235d9951bd35b5fdb40c
Phil <philipp.teschl@gmx.at>
2017-05-20 15:11:36 +0200

    Stopwatch Start

--- app/src/main/java/at/c02/tempus/app/ui/booking/FragmentBooking.java
+++ app/src/main/java/at/c02/tempus/app/ui/booking/FragmentBooking.java
@@ -19,6 +19,9 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import nucleus.view.NucleusSupportFragment;
+import android.os.SystemClock;
+import android.os.Handler;
+import android.widget.TextView;
 
 
 /**
@@ -33,44 +36,96 @@ public class FragmentBooking extends NucleusSupportFragment<FragmentBookingPrese
     @BindView(R.id.btnRecordButton)
     protected Button btnRecordButton;
 
-   private ArrayAdapter<ProjectEntity> adapter;
+    @BindView(R.id.textView)
+    protected TextView textView;
+
+    private ArrayAdapter<ProjectEntity> adapter;
+
+    //Stopwatch
+    long StartTime, MillisecondTime, TimeBuff, UpdateTime = 0L;
+    Handler handler;
+    int Seconds, Minutes, MilliSeconds;
 
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_booking, container, false);
-        ButterKnife.bind(this,root);
-       adapter = new ArrayAdapter<>(this.getContext(), R.layout.support_simple_spinner_dropdown_item);
-       cbProject.setAdapter(adapter);
-       cbProject.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-          @Override
-        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-          getPresenter().setProject(adapter.getItem(position));
-      }
-      @Override
-      public void onNothingSelected(AdapterView<?> parent) {
-        getPresenter().setProject(null);
-      }
-      });
+        ButterKnife.bind(this, root);
+       handler = new Handler();
+        adapter = new ArrayAdapter<>(this.getContext(), R.layout.support_simple_spinner_dropdown_item);
+        cbProject.setAdapter(adapter);
+        cbProject.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                getPresenter().setProject(adapter.getItem(position));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                getPresenter().setProject(null);
+            }
+        });
 
         return root;
     }
 
     @OnClick(R.id.btnRecordButton)
-    protected  void onRecordClick()
-    {
+    protected void onRecordClick() {
         //do something...
     }
 
+    @OnClick(R.id.btnStopWatch)
+    protected void onRecordClick2() {
 
-    public void updateProjects(List<ProjectEntity> projects) {
-        adapter.clear();
-        adapter.addAll(projects);
+       runnable = new Runnable() {
+           @Override
+           public void run() {
+               MillisecondTime = SystemClock.uptimeMillis() - StartTime;
+
+               UpdateTime = TimeBuff + MillisecondTime;
+
+               Seconds = (int) (UpdateTime / 1000);
+
+               Minutes = Seconds / 60;
+
+               Seconds = Seconds % 60;
+
+               MilliSeconds = (int) (UpdateTime % 1000);
+
+               textView.setText("" + Minutes + ":"
+                       + String.format("%02d", Seconds) + ":"
+                       + String.format("%03d", MilliSeconds));
+
+               handler.postDelayed(this, 0);
+           }
+       };
+       runnable.run();
+        StartTime = SystemClock.uptimeMillis();
+        handler.postDelayed(runnable, 0);
+
+
+        //do something...
     }
-    public void onError(Throwable error) {
-        Toast.makeText(this.getContext(), error.getMessage(), Toast.LENGTH_LONG).show();
-    }
+
+    public Runnable runnable = new Runnable() {
+
+        public void run() {
+
+
+        }
+    };
+
+        public void updateProjects(List<ProjectEntity> projects) {
+            adapter.clear();
+            adapter.addAll(projects);
+        }
+
+        public void onError(Throwable error) {
+            Toast.makeText(this.getContext(), error.getMessage(), Toast.LENGTH_LONG).show();
+        }
+
 
 
 }
+
