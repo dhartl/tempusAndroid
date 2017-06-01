7614fd3980da42c57b555ba8ba4ce23e5cd3f7ab
Daniel Hartl <daniel_hartl@gmx.at>
2017-06-01 21:02:46 +0200

    Charts sind lauffähig

--- app/src/main/java/at/c02/tempus/app/ui/report/ReportActivity.java
+++ app/src/main/java/at/c02/tempus/app/ui/report/ReportActivity.java
@@ -2,36 +2,24 @@ package at.c02.tempus.app.ui.report;
 
 import android.app.DatePickerDialog;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.widget.Button;
 
-import java.util.ArrayList;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.PieData;
+
 import java.util.Calendar;
 import java.util.Date;
-import java.util.List;
 
 import at.c02.tempus.R;
-import at.c02.tempus.app.ui.bookinglist.BookingActivityPresenter;
-import at.c02.tempus.app.ui.bookinglist.BookingListItem;
-import at.c02.tempus.db.entity.BookingEntity;
 import at.c02.tempus.utils.DateUtils;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import nucleus.factory.RequiresPresenter;
 import nucleus.view.NucleusAppCompatActivity;
-import eu.davidea.flexibleadapter.FlexibleAdapter;
-
-import android.graphics.Color;
-
-import com.github.mikephil.charting.charts.BarChart;
-import com.github.mikephil.charting.charts.HorizontalBarChart;
-import com.github.mikephil.charting.data.BarData;
-import com.github.mikephil.charting.data.BarDataSet;
-import com.github.mikephil.charting.data.BarEntry;
-import com.github.mikephil.charting.utils.ColorTemplate;
 
 
 
@@ -45,52 +33,55 @@ public class ReportActivity extends NucleusAppCompatActivity<ReportPresenter> {
     @BindView(R.id.btnDateStart)
     protected Button btnDateStart;
 
-
-    @BindView(R.id.btnreport)
-    protected Button btnreport;
-
     @BindView(R.id.chart)
-    protected HorizontalBarChart chart;
-
-    /*
-    @BindView(R.id.recyclerView)
-    protected  RecyclerView recyclerView;
-*/
-
-    FlexibleAdapter<BookingListItem> adapter;
+    protected PieChart chart;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTitle("Zeit pro Projekt");
         setContentView(R.layout.activity_report);
         ButterKnife.bind(this);
         setSupportActionBar(toolbar);
 
-        BarChart chart = (BarChart) findViewById(R.id.chart);
 
-        BarData data = new BarData(getXAxisValues(), getDataSet());
-        chart.setData(data);
-        chart.setDescription("My Chart");
-        chart.animateXY(2000, 2000);
-        chart.invalidate();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        chart.setDescription(null);
+        chart.setDrawHoleEnabled(true);
+        chart.setHoleRadius(7);
+        chart.setTransparentCircleRadius(10);
+
+        Legend legend = chart.getLegend();
+        legend.setHorizontalAlignment(Legend.LegendHorizontalAlignment.CENTER);
+        legend.setVerticalAlignment(Legend.LegendVerticalAlignment.BOTTOM);
+        legend.setFormSize(14);
+        legend.setTextSize(14);
+        legend.setOrientation(Legend.LegendOrientation.HORIZONTAL);
+        legend.setXEntrySpace(5);
+
     }
 
-
-    @OnClick(R.id.btnreport)
-    protected void onBtnreportClick() { getPresenter().createReport();}
-
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+        return false;
+    }
 
     @OnClick(R.id.btnDateStart)
     protected void onBtnDateStartClick() {
-        Date date = getPresenter().getModel().getBeginDate();
+        Date date = getPresenter().getStartDate();
         if (date == null) {
             date = new Date();
         }
         Calendar calendar = DateUtils.dateToCalendar(date);
         DatePickerDialog datePickerDialog = new DatePickerDialog(this, 0,
                 (view, year, month, dayOfMonth) ->
-                        getPresenter().setStartDate(year, month, dayOfMonth),
+                        getPresenter().createReport(year, month, dayOfMonth),
                 calendar.get(Calendar.YEAR),
                 calendar.get(Calendar.MONTH),
                 calendar.get(Calendar.DAY_OF_MONTH));
@@ -115,97 +106,10 @@ public class ReportActivity extends NucleusAppCompatActivity<ReportPresenter> {
     }
 
 
-//public ArrayList<BarDataSet> getDataSet(List<BookingEntity> bookings) {
-    public ArrayList<BarDataSet> getDataSet() {
-
-       // List<BookingEntity> bookings = null;
-
-        ArrayList<BarDataSet> dataSets = null;
-
-        ArrayList<BarEntry> valueSet1 = new ArrayList<>();
-
-       /* for (BookingEntity booking : bookings) {
-
-            //if --> Start date missing /filter startDate
-
-            //duration
-            Date d1 = booking.getBeginDate();
-            Date d2 = booking.getEndDate();
-            long sum =  d2.getTime() - d1.getTime() ;
-
-
-        //    valueSet1.add(new BarEntry(booking.getProject(), sum);
-
-
-        }*/
-
-    /* adapter?
-        if (adapter == null) {
-            adapter = new FlexibleAdapter<>(valueSet);
-            // chart.setAdapter(adapter);
-
-        } else {
-            adapter.updateDataSet(listItems);
-        }
-    */
-
-
-       //valueSet in dataSEt
-
-
-
-    BarEntry v1e1 = new BarEntry(110.000f, 0); // Jan
-        valueSet1.add(v1e1);
-    BarEntry v1e2 = new BarEntry(40.000f, 1); // Feb
-        valueSet1.add(v1e2);
-    BarEntry v1e3 = new BarEntry(60.000f, 2); // Mar
-        valueSet1.add(v1e3);
-    BarEntry v1e4 = new BarEntry(30.000f, 3); // Apr
-        valueSet1.add(v1e4);
-    BarEntry v1e5 = new BarEntry(90.000f, 4); // May
-        valueSet1.add(v1e5);
-    BarEntry v1e6 = new BarEntry(100.000f, 5); // Jun
-        valueSet1.add(v1e6);
-
-    ArrayList<BarEntry> valueSet2 = new ArrayList<>();
-    BarEntry v2e1 = new BarEntry(150.000f, 0); // Jan
-        valueSet2.add(v2e1);
-    BarEntry v2e2 = new BarEntry(90.000f, 1); // Feb
-        valueSet2.add(v2e2);
-    BarEntry v2e3 = new BarEntry(120.000f, 2); // Mar
-        valueSet2.add(v2e3);
-    BarEntry v2e4 = new BarEntry(60.000f, 3); // Apr
-        valueSet2.add(v2e4);
-    BarEntry v2e5 = new BarEntry(20.000f, 4); // May
-        valueSet2.add(v2e5);
-    BarEntry v2e6 = new BarEntry(80.000f, 5); // Jun
-        valueSet2.add(v2e6);
-
-    BarDataSet barDataSet1 = new BarDataSet(valueSet1, "Brand 1");
-        barDataSet1.setColor(Color.rgb(0, 155, 0));
-    BarDataSet barDataSet2 = new BarDataSet(valueSet2, "Brand 2");
-        barDataSet2.setColors(ColorTemplate.COLORFUL_COLORS);
-
-    dataSets = new ArrayList<>();
-        dataSets.add(barDataSet1);
-        dataSets.add(barDataSet2);
-        return dataSets;
+    public void publishData(Date startDate, PieData pieData) {
+        updateStartDate(startDate);
+        chart.setData(pieData);
+        chart.highlightValues(null);
+        chart.invalidate();
     }
-
-
-
-    private ArrayList<String> getXAxisValues() {
-        ArrayList<String> xAxis = new ArrayList<>();
-        xAxis.add("JAN");
-        xAxis.add("FEB");
-        xAxis.add("MAR");
-        xAxis.add("APR");
-        xAxis.add("MAI");
-        xAxis.add("JUN");
-        return xAxis;
-    }
-
-
-
-
 }
--- app/src/main/java/at/c02/tempus/app/ui/report/ReportPresenter.java
+++ app/src/main/java/at/c02/tempus/app/ui/report/ReportPresenter.java
@@ -1,31 +1,41 @@
 package at.c02.tempus.app.ui.report;
 
+import android.graphics.Color;
+
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.formatter.IValueFormatter;
+import com.github.mikephil.charting.utils.ColorTemplate;
+import com.github.mikephil.charting.utils.ViewPortHandler;
+
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
 import javax.inject.Inject;
+
 import at.c02.tempus.app.TempusApplication;
 import at.c02.tempus.db.entity.BookingEntity;
-import at.c02.tempus.service.BookingService;
 import at.c02.tempus.service.ReportService;
 import at.c02.tempus.utils.DateUtils;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import nucleus.presenter.Presenter;
-import java.util.List;
-
 
 
 public class ReportPresenter extends Presenter<ReportActivity> {
 
 
-    private BookingEntity model = new BookingEntity();
-
     @Inject
     protected ReportService reportService;
 
-    @Inject
-    protected BookingService bookingService;
+    protected PieData pieData;
 
-
-    protected List<BookingEntity> bookings;
+    protected Date startDate;
 
 
     public ReportPresenter() {
@@ -33,46 +43,80 @@ public class ReportPresenter extends Presenter<ReportActivity> {
     }
 
 
-    public BookingEntity getModel() {
-        return model;
-    }
-
-    public void setModel(BookingEntity model) {
-        this.model = model;
-    }
-
-
-    public void createReport() {
-        bookingService.getBookings().observeOn(AndroidSchedulers.mainThread())
+    public void createReport(int year, int month, int dayOfMonth) {
+        startDate = DateUtils.setDate(null, year, month, dayOfMonth);
+        reportService
+                .findBookings(startDate)
+                .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(this::onBookingsLoaded);
-    }
-
-
-    private void onBookingsLoaded(List<BookingEntity> bookings) {
-        this.bookings = bookings;
-        if (getView() != null) {
-            getView().getDataSet();
-            //getView().getDataSet(bookings);
+        if(getView() != null) {
+            getView().updateStartDate(startDate);
         }
     }
 
 
-    public void setStartDate(int year, int month, int day) {
-        Date newBeginDate = DateUtils.setDate(model.getBeginDate(), year, month, day);
-        model.setBeginDate(newBeginDate);
-       getView().updateStartDate(newBeginDate);
+    private void onBookingsLoaded(List<BookingEntity> bookings) {
+
+        Map<String, Long> projectToDuration = new HashMap<>();
+        for (BookingEntity booking : bookings) {
+            Long duration = projectToDuration.get(booking.getProject().getName());
+            if (duration == null) {
+                duration = getDurationInMinutes(booking);
+            } else {
+                duration += getDurationInMinutes(booking);
+            }
+            projectToDuration.put(booking.getProject().getName(), duration);
+        }
+
+        List<PieEntry> entries = new ArrayList<>();
+        for (Map.Entry<String, Long> mapEntry : projectToDuration.entrySet()) {
+            PieEntry entry = new PieEntry(mapEntry.getValue());
+            entry.setLabel(mapEntry.getKey());
+            entries.add(entry);
+        }
+
+        PieDataSet dataSet = new PieDataSet(entries, null);
+        dataSet.setSliceSpace(3);
+        dataSet.setSelectionShift(5);
+
+        dataSet.setColors(ColorTemplate.PASTEL_COLORS);
+
+        pieData = new PieData(dataSet);
+        pieData.setValueFormatter(new IValueFormatter() {
+            @Override
+            public String getFormattedValue(float value, Entry entry, int dataSetIndex, ViewPortHandler viewPortHandler) {
+                long valueMinutes = (long) value;
+                if (valueMinutes < 60) {
+                    return String.format("%02d", valueMinutes);
+                } else {
+                    long minutes = valueMinutes % 60;
+                    long hours = TimeUnit.MINUTES.toHours(valueMinutes);
+                    return String.format("%d:%02d", hours, minutes);
+                }
+            }
+        });
+        pieData.setValueTextSize(14);
+        pieData.setValueTextColor(Color.WHITE);
+
+        if (getView() != null) {
+            getView().publishData(startDate, pieData);
+        }
+    }
+
+    private long getDurationInMinutes(BookingEntity booking) {
+        return TimeUnit.MILLISECONDS.toMinutes(booking.getEndDate().getTime() - booking.getBeginDate().getTime());
     }
 
 
     @Override
     protected void onTakeView(ReportActivity reportActivity) {
         super.onTakeView(reportActivity);
-            if (bookings != null) {
-                getView().getDataSet();
-                // getView().getDataSet(bookings);
-            }
+        if (getView() != null && pieData != null) {
+            getView().publishData(startDate, pieData);
         }
+    }
 
-
-
+    public Date getStartDate() {
+        return startDate;
+    }
 }
